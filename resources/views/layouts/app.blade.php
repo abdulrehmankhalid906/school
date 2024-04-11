061acc8ccd2f6759d6f1ffd6d2f3644ffe080a9a
abdulrehmankhalid906 <abdulrehmankhalid906@gmail.com>
2024-04-11 12:01:11 +0500

    Setup Twak.io chat

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,19 @@
   <script src="{{ asset('admin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
   <script src="{{ asset('admin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
   <script src="{{ asset('admin/assets/js/main.js') }}"></script>
-</body>
 
+  {{-- To integrate Twak.io (just open web and create this script and add it here) --}}
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/661784291ec1082f04e11ae1/1hr5tohgo';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+
+</body>
 </html>
